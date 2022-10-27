ab5e6f5f560ff76b84821bcf908ab89250640b62
Dang Dinh Quoc Trung <dangtrung96x@gmail.com>
2022-10-27 23:32:52 +0700

    Fix setup

--- mmdata/renderer/gl/init_gl.py
+++ mmdata/renderer/gl/init_gl.py
@@ -1,12 +1,14 @@
+
 _glut_window = None
 _context_inited = None
 
+
 def initialize_GL_context(width=512, height=512, egl=False):
-    '''
+    """
     default context uses GLUT
-    '''
+    """
     if not egl:
-        import OpenGL.GLUT as GLUT      
+        import OpenGL.GLUT as GLUT
         display_mode = GLUT.GLUT_DOUBLE | GLUT.GLUT_RGB | GLUT.GLUT_DEPTH
         global _glut_window
         if _glut_window is None:
@@ -21,4 +23,3 @@ def initialize_GL_context(width=512, height=512, egl=False):
         if _context_inited is None:
             create_opengl_context((width, height))
             _context_inited = True
-
